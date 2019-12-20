e3c0d5ed59394fe7089e3693c165fb52e976b340
SanderCC <58855319+SanderCC@users.noreply.github.com>
2019-12-20 16:38:37 +0100

    Keuze rol bij speler

--- src/be/kdg/spel/Spel.java
+++ src/be/kdg/spel/Spel.java
@@ -6,4 +6,12 @@ import java.util.ArrayList;
 
 public class Spel {
     protected ArrayList<Speler> spelers = new ArrayList<Speler>();
+
+    public void voegSpelerToe(Speler speler){
+        this.spelers.add(speler);
+    }
+
+    public ArrayList<Speler> getSpelers() {
+        return spelers;
+    }
 }
