ada3b8a715f853d20cd4529c5ad900abebd384f7
SanderCC <58855319+SanderCC@users.noreply.github.com>
2020-02-04 11:59:37 +0100

    Bord aangemaakt in main

--- src/be/kdg/Main.java
+++ src/be/kdg/Main.java
@@ -3,7 +3,10 @@ package be.kdg;
 import be.kdg.spel.Spel;
 import be.kdg.speler.Rol;
 import be.kdg.speler.Speler;
+import be.kdg.vak.Start;
+import be.kdg.vak.Vak;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,5 +15,13 @@ public class Main {
     public static void main(String[] args) {
         Spel spel = new Spel();
         spel.maakSpelers();
+
+        Vak[] bord = new Vak[38];
+        bord[0] = new Start();
+        bord[9] = new Start();
+        for(int i = 0;i<38;i++){
+
+            bord[i] =
+        }
     }
 }
