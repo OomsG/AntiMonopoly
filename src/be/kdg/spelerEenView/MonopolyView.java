0007c6db124a72a747421d7acb41267a25befb67
justnathan321 <justnathan321@gmail.com>
2020-03-03 12:34:24 +0100

    Registratie form toegevoegd VOLLEDIG

--- src/be/kdg/spelerEenView/MonopolyView.java
+++ src/be/kdg/spelerEenView/MonopolyView.java
@@ -30,10 +30,14 @@ public class MonopolyView extends GridPane
     }
 
     private void initialiseNodes() {
-        tfNaamSpeler1 = new javafx.scene.control.TextField("Naam speler 1");
-        tfNaamSpeler2 = new javafx.scene.control.TextField("Naam speler 2");
-        tfNaamSpeler3 = new javafx.scene.control.TextField("Naam speler 3");
-        tfNaamSpeler4 = new javafx.scene.control.TextField("Naam speler 4");
+        tfNaamSpeler1 = new javafx.scene.control.TextField();
+        tfNaamSpeler1.setPromptText("Naam speler 1");
+        tfNaamSpeler2 = new javafx.scene.control.TextField();
+        tfNaamSpeler2.setPromptText("Naam speler 2");
+        tfNaamSpeler3 = new javafx.scene.control.TextField();
+        tfNaamSpeler3.setPromptText("Naam speler 3");
+        tfNaamSpeler4 = new javafx.scene.control.TextField();
+        tfNaamSpeler4.setPromptText("Naam speler 4");
         btnBevestigSpelers = new javafx.scene.control.Button("Spelers bevestigen");
         mainLogo = new Image("/logo.png");
         lbMainLogo = new Label("", new ImageView(mainLogo));
@@ -52,7 +56,6 @@ public class MonopolyView extends GridPane
         this.add(tfNaamSpeler3, 0, 4);
         this.add(tfNaamSpeler4, 1, 4);
         this.add(btnBevestigSpelers, 0, 5);
-
         this.tfNaamSpeler1.setMaxWidth(200);
         this.tfNaamSpeler2.setMaxWidth(200);
         this.tfNaamSpeler3.setMaxWidth(200);
@@ -61,8 +64,6 @@ public class MonopolyView extends GridPane
         GridPane.setMargin(tfNaamSpeler2, new Insets(20, 20, 20, 10));
         GridPane.setMargin(tfNaamSpeler3, new Insets(5, 20, 20, 10));
         GridPane.setMargin(tfNaamSpeler4, new Insets(5, 20, 20, 10));
-
-
         GridPane.setMargin(btnBevestigSpelers, new Insets(5, 20, 20, 150));
 
 
