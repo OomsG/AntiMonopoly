5e7192ade2481bce29dd915db28b3d8fb8223775
justnathan321 <justnathan321@gmail.com>
2020-03-06 13:23:50 +0100

    styles en view

--- src/be/kdg/view/game/GameView.java
+++ src/be/kdg/view/game/GameView.java
@@ -17,6 +17,10 @@ public class GameView extends GridPane {
     private ImageView speelBord;
     private ImageView scheidingsLijn;
     private Label lblinstructiesConsoleBox;
+    private Label lblNaamBeurt;
+    private Button btnDobbel;
+    private Button btnKoopGrond;
+    private Button btnBouwen;
     private javafx.scene.control.Button btnBeurt;
 
 
@@ -32,6 +36,10 @@ public class GameView extends GridPane {
         scheidingsLijn = new ImageView(new Image("scheidingslijn.png"));
         lblinstructiesConsoleBox = new Label("labelInstructiesConsoleBox");
         btnBeurt = new Button("Volgende beurt");
+        btnBouwen = new Button("Bouwen");
+        btnDobbel = new Button("Dobbel");
+        btnKoopGrond = new Button("Kopen");
+        lblNaamBeurt = new Label("lblNaamBeurt");
 
     }
 
@@ -40,19 +48,39 @@ public class GameView extends GridPane {
         this.add(speelBord, 1, 0);
         speelBord.setFitHeight(900);
         speelBord.setFitWidth(1000);
-        GridPane.setMargin(speelBord, new Insets(15, 20, 15, 45));
         this.add(scheidingsLijn, 1, 0);
-        GridPane.setMargin(scheidingsLijn, new Insets(0, 20, 0, 1100));
         this.add(lblinstructiesConsoleBox, 2, 0);
-        GridPane.setMargin(lblinstructiesConsoleBox, new Insets(0, 45, 800, 45));
         this.add(btnBeurt, 2, 0);
+        this.add(btnDobbel,2,0);
 
         // Margins & Padding
-        GridPane.setMargin(btnBeurt, new Insets(150, 45, 200, 70));
-        btnBeurt.setPadding(new Insets(20, 20, 20, 20));
+        GridPane.setMargin(speelBord, new Insets(15, 20, 15, 45));
+
+        GridPane.setMargin(scheidingsLijn, new Insets(0, 20, 0, 1100));
+
+        GridPane.setMargin(lblinstructiesConsoleBox, new Insets(0, 45, 800, 45));
+
+        GridPane.setMargin(btnBeurt, new Insets(500, -50, 200, 30));
+        btnBeurt.setPadding(new Insets(25, 32, 32, 25));
+
+        GridPane.setMargin(btnDobbel, new Insets(300, -50, 200, 30));
+        btnDobbel.setPadding(new Insets(25, 64, 25, 65));
+
 
         // CSS
-        lblinstructiesConsoleBox.setStyle("-fx-effect: dropshadow(one-pass-box, black, 100, 0.1, 1, 1);");
+
+        btnBeurt.setStyle("-fx-background-color: \n" +
+                "        #a6b5c9,\n" +
+                "        linear-gradient(#303842 0%, #3e5577 20%, #375074 100%),\n" +
+                "        linear-gradient(#768aa5 0%, #849cbb 5%, #5877a2 50%, #486a9a 51%, #4a6c9b 100%);-fx-text-fill: WHITE;");
+        btnBeurt.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+
+
+        btnDobbel.setStyle("-fx-background-color: \n" +
+                "        #a6b5c9,\n" +
+                "        linear-gradient(#303842 0%, #3e5577 20%, #375074 100%),\n" +
+                "        linear-gradient(#768aa5 0%, #849cbb 5%, #5877a2 50%, #486a9a 51%, #4a6c9b 100%);-fx-text-fill: WHITE;");
+        btnDobbel.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
 
     }
 }
