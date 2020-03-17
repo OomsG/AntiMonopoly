edc2538b436280b80e240cae029649f070ce267c
SanderCC <58855319+SanderCC@users.noreply.github.com>
2020-03-17 15:32:54 +0100

    toggle van koop knop laten werken

--- src/be/kdg/view/game/GamePresenter.java
+++ src/be/kdg/view/game/GamePresenter.java
@@ -4,22 +4,21 @@ package be.kdg.view.game;
 import be.kdg.spelLogica.spel.Spel;
 import be.kdg.spelLogica.speler.Speler;
 import be.kdg.spelLogica.vak.Grond;
-import be.kdg.view.start.StartPresenter;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 
 public class GamePresenter {
     private GameView view;
-    private Spel model;
+    private Spel spel;
     private boolean heeftGedobbeld = true;
+    private boolean kanGrondKopen = false;
     int i = -1;
 
     public GamePresenter(Spel model, GameView view) {
         this.view = view;
-        this.model = model;
+        this.spel = model;
         addEventHandlers();
         view.getBtnBeurt().fire();
     }
@@ -32,19 +31,21 @@ public class GamePresenter {
             public void handle(ActionEvent event) {
                 if(heeftGedobbeld == true){
                     heeftGedobbeld = false;
+                    kanGrondKopen = false;
+                    view.toggleKoopGrond(false);
                     i++;
-                    if(i >= model.getSpelers().size()){
+                    if(i >= spel.getSpelers().size()){
                         i = 0;
                     }
-                    for(Speler speler : model.getSpelers()){
+                    for(Speler speler : spel.getSpelers()){
                         speler.setBeurt(false);
                     }
-                    model.getSpelers().get(i).setBeurt(true);
-                    view.voegToeAanConsoleBox("Beurt doorgegeven aan "+model.getSpelers().get(i).getNaam());
-                    String tekst = "Speler: "+ model.getSpelers().get(i).getNaam()
-                            +"\nRol: "+ model.getSpelers().get(i).getRol()
-                            +"\nSaldo: €"+ model.getSpelers().get(i).getScore()
-                            +"\nPositie: "+ model.getSpelers().get(i).getPositie();
+                    spel.getSpelers().get(i).setBeurt(true);
+                    view.voegToeAanConsoleBox("Beurt doorgegeven aan "+ spel.getSpelers().get(i).getNaam());
+                    String tekst = "Speler: "+ spel.getSpelers().get(i).getNaam()
+                            +"\nRol: "+ spel.getSpelers().get(i).getRol()
+                            +"\nSaldo: €"+ spel.getSpelers().get(i).getScore()
+                            +"\nPositie: "+ spel.getSpelers().get(i).getPositie();
                     view.getTaNaamBeurt().setText(tekst);
                 } else {
                     view.voegToeAanConsoleBox("Je moet eerst dobbelen!");
@@ -55,39 +56,42 @@ public class GamePresenter {
             @Override
             public void handle(ActionEvent event) {
                 if(heeftGedobbeld == false){
-                    view.toggleKoopGrond(false);
                     view.voegToeAanConsoleBox("Gooit de dobbelstenen (spannend)");
                     Speler mySpeler = null;
                     System.out.println("Start dobbel");
-                    for(Speler speler : model.getSpelers()){
+                    for(Speler speler : spel.getSpelers()){
                         if(speler.isBeurt() == true){
                             mySpeler = speler;
                         }
                     }
-                    int newPos = model.dobbelNewPos() + mySpeler.getPositie();
+                    int newPos = spel.dobbelNewPos() + mySpeler.getPositie();
                     if(newPos >= 39){
                         newPos -= 39;
-                    }
-
-                    if(model.getVak(newPos).getSoort() == "grond"){
-                        Grond huidigVak = (Grond)model.getVak(newPos);
-                        if(!huidigVak.isGekocht() && huidigVak.getPrijs() <= mySpeler.getScore()){
-
-                        } else if(huidigVak.isGekocht() && (huidigVak.getPrijs()*0.3)+1 <= mySpeler.getScore()){
-
-                        } else if(mySpeler.getScore() < 0) {
-
-                        }
                     }
 
                     System.out.println("New position: "+newPos);
                     mySpeler.setPositie(newPos);
                     heeftGedobbeld = true;
 
-                    String tekst = "Speler: "+ model.getSpelers().get(i).getNaam()
-                            +"\nRol: "+ model.getSpelers().get(i).getRol()
-                            +"\nSaldo: €"+ model.getSpelers().get(i).getScore()
-                            +"\nPositie: "+ model.getSpelers().get(i).getPositie();
+                    if(spel.getVak(newPos).getSoort() == "grond"){
+                        Grond huidigVak = (Grond) spel.getVak(newPos);
+                        if(!huidigVak.isGekocht() && huidigVak.getPrijs() <= mySpeler.getScore()){
+                            view.toggleKoopGrond(true);
+                            kanGrondKopen = true;
+                            System.out.println("Mogelijkheid om huis te kopen: JA");
+                        } else if(huidigVak.isGekocht() && (huidigVak.getPrijs()*0.3)+1 <= mySpeler.getScore()){
+
+                            System.out.println("Speler moet boete betalen");
+                        } else if(mySpeler.getScore() < 0) {
+
+                            System.out.println("Speler kan boete niet betalen..");
+                        }
+                    }
+
+                    String tekst = "Speler: "+ spel.getSpelers().get(i).getNaam()
+                            +"\nRol: "+ spel.getSpelers().get(i).getRol()
+                            +"\nSaldo: €"+ spel.getSpelers().get(i).getScore()
+                            +"\nPositie: "+ spel.getSpelers().get(i).getPositie();
                     view.getTaNaamBeurt().setText(tekst);
 
 
